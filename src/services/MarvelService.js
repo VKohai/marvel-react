939bc555c8fd017a3f4e4343a84c25282a8a4bf7
Vladislav Kohai <cherry.designwork@gmail.com>
2023-06-27 22:06:35 +0300

    Random character generator

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,6 @@
 class MarvelService {
-    __BASE_URL = 'https://gateway.marvel.com:443/v1/public/';
-    __API_KEY = 'a8d09a552b37f6754f38c96deae0ac1f';
+    #BASE_URL = 'https://gateway.marvel.com:443/v1/public/';
+    #API_KEY = 'a8d09a552b37f6754f38c96deae0ac1f';
 
     request = async (url) => {
         const response = await fetch(url);
@@ -12,12 +12,25 @@ class MarvelService {
 
     // https://gateway.marvel.com:443/v1/public/characters?apikey=
     getCharacters = async () => {
-        return await this.request(`${this.__BASE_URL}characters?apikey=${this.__API_KEY}`);
+        const response = await this.request(`${this.#BASE_URL}characters?apikey=${this.#API_KEY}`);
+        return response.data.results.map(this.#parseCharacter);
     }
 
     // https://gateway.marvel.com:443/v1/public/characters/2?apikey=
     getCharacterById = async (id) => {
-        return await this.request(`${this.__BASE_URL}characters/${id}?apikey=${this.__API_KEY}`);
+        const response = await this.request(`${this.#BASE_URL}characters/${id}?apikey=${this.#API_KEY}`);
+        return this.#parseCharacter(response.data.results[0]);
+    }
+
+    #parseCharacter = (character) => {
+        const thumbnailPath = `${character.thumbnail.path}.${character.thumbnail.extension}`;
+        return {
+            name: character.name,
+            description: character.description,
+            thumbnail: thumbnailPath,
+            homepage: character.urls[0].url,
+            wiki: character.urls[1].url
+        };
     }
 }
 
