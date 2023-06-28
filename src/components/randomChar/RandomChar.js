59ebe31145545fad1cc901a135b5e561b7cd6948
Vladislav Kohai <cherry.designwork@gmail.com>
2023-06-28 09:52:33 +0300

    optimisation requests

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -15,10 +15,13 @@ class RandomChar extends Component {
             error: false
         };
         this.marvelService = new MarvelService();
-        this.updateChar();
     }
 
     // #region methods
+    componentDidMount() {
+        this.updateChar();
+    }
+
     updateChar = () => {
         const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
         this.marvelService
